1831ab97a081de3b79b2f7f102c667cc2ecf0a32
Zayne <star1582@naver.com>
2024-02-08 16:55:17 +0900

    Create the rest of Entities

--- src/main/kotlin/com/zayne/portfolio/domain/entity/ExperienceDetail.kt
+++ src/main/kotlin/com/zayne/portfolio/domain/entity/ExperienceDetail.kt
@@ -1,16 +1,21 @@
 package com.zayne.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
-class ExperienceDetail : BaseEntity() {
+class ExperienceDetail(content: String, isActive: Boolean) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "experience_detail_id")
     var id: Long? = null
+
+    var content: String = content
+
+    var isActive: Boolean = isActive
+
+    fun udpate(content: String, isActive: Boolean) {
+        this.content = content
+        this.isActive = isActive
+    }
 }
--- src/main/kotlin/com/zayne/portfolio/domain/entity/ProjectSkill.kt
+++ src/main/kotlin/com/zayne/portfolio/domain/entity/ProjectSkill.kt
@@ -1,16 +1,20 @@
 package com.zayne.portfolio.domain.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
-class ProjectSkill : BaseEntity() {
+class ProjectSkill(project: Project, skill: Skill) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "project_skill_id")
     var id: Long? = null
+
+    @ManyToOne(targetEntity = Project::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    var project: Project = project
+
+    @ManyToOne(targetEntity = Skill::class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "skill_id", nullable = false)
+    var skill: Skill = skill
 }
