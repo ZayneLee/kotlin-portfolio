764ca745cfd9a7b09afc5866e8b22493f7474f62
Zayne <star1582@naver.com>
2024-02-08 09:13:43 +0900

    first commit

---
+++ src/test/kotlin/com/zayne/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zayne.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
