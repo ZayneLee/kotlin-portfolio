6f934d168820194a5c92b2c68046c1cb9696c27b
Zayne <star1582@naver.com>
2024-02-09 09:55:20 +0900

    Improve ExperienceRepository Performance #8

--- src/main/kotlin/com/zayne/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/zayne/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,9 +2,14 @@ package com.zayne.portfolio.domain.repository
 
 import com.zayne.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
+
+    @Query("select e from Experience e join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("select e from Experience e join fetch e.details where e.id = :id")
     override fun findById(id: Long): Optional<Experience>
 }
