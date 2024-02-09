f21d867a3025d30e48e34c0f4b6294c96a6bf55a
Zayne <star1582@naver.com>
2024-02-09 20:01:22 +0900

    Create class #10

---
+++ src/main/kotlin/com/zayne/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -0,0 +1,9 @@
+package com.zayne.portfolio.presentation.interceptor
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+
+@Configuration
+class PresentationInterceptorConfiguration {
+
+}
---
+++ src/main/kotlin/com/zayne/portfolio/presentation/service/PresentationService.kt
@@ -0,0 +1,7 @@
+package com.zayne.portfolio.presentation.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class PresentationService {
+}
