e847e0229fd564224f82f4d1115debbcbd7f41fb
Zayne <star1582@naver.com>
2024-02-14 13:26:58 +0900

    Interceptor Development #34

--- src/main/kotlin/com/zayne/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
+++ src/main/kotlin/com/zayne/portfolio/presentation/interceptor/PresentationInterceptorConfiguration.kt
@@ -1,9 +1,16 @@
 package com.zayne.portfolio.presentation.interceptor
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class PresentationInterceptorConfiguration {
-
+class PresentationInterceptorConfiguration(
+    private val presentationInterceptor: PresentationInterceptor
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(presentationInterceptor)
+            .addPathPatterns("/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/admin/**", "/h2**", "/favicon.ico", "/error")
+    }
 }
