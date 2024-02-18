8cad478dd65387188f4dc8f41b7e0b6efb9d5715
Zayne <star1582@naver.com>
2024-02-18 11:17:31 +0900

    Exception and Advice development #39

--- src/main/kotlin/com/zayne/portfolio/admin/advice/AdminApiControllerAdvice.kt
+++ src/main/kotlin/com/zayne/portfolio/admin/advice/AdminApiControllerAdvice.kt
@@ -1,7 +1,38 @@
 package com.zayne.portfolio.admin.advice
 
+import com.zayne.portfolio.admin.exception.AdminException
+import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class AdminApiControllerAdvice {
+
+    val log = LoggerFactory.getLogger(AdminApiControllerAdvice::class.java)
+
+    @ExceptionHandler
+    fun handleException(e: AdminException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.status(e.httpStatus).body(e.message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: MethodArgumentNotValidException): ResponseEntity<String> {
+        log.info(e.message, e)
+
+        val fieldError = e.bindingResult.fieldErrors[0]
+        val message = "[${fieldError.field}] ${fieldError.defaultMessage}"
+
+        return ResponseEntity.badRequest().body(message)
+    }
+
+    @ExceptionHandler
+    fun handleException(e: Exception):ResponseEntity<String> {
+        log.info(e.message, e)
+
+        return ResponseEntity.internalServerError().body("System Error")
+    }
 }
