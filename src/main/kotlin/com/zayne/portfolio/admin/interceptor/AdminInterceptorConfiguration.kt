b9ab322180d6111a1e4cf128440f7377d5f236de
Zayne <star1582@naver.com>
2024-02-20 11:19:23 +0900

    Interceptor development #43

--- src/main/kotlin/com/zayne/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/zayne/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -7,8 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class AdminInterceptorConfiguration(
     val adminInterceptor: AdminInterceptor
-):WebMvcConfigurer {
+) : WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/h2**")
 
     }
 }
